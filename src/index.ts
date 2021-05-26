dd92e323fef7587c08f7119a4f4fe76a2f466ea3
Charlie Carolan <charliecarolan1999@gmail.com>
2021-05-26 22:18:08 +0100

    Now in 3D

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,10 @@ interface ProgramInfo {
 	uniformLocations: Record<'projectionMatrix' | 'modelViewMatrix', WebGLUniformLocation | null>,
 };
 
-type Buffers = Record<'position' | 'color', WebGLBuffer>
+type Buffers = Record<'position' | 'color' | 'indices', WebGLBuffer>
 
 let then = 0;
-let squareRotation = 0;
+let cubeRotation = 0;
 
 function getCanvasElement(query: string | WebGL2RenderingContext): HTMLCanvasElement {
 	if (typeof query === "string") {
@@ -89,28 +89,82 @@ function initBuffers(gl: WebGL2RenderingContext) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
 	const positions = [
-		-1.0, 1.0,
-		1.0, 1.0,
-		-1.0, -1.0,
-		1.0, -1.0,
+		// Front face
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, 1.0,
+
+		// Back face
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+
+		// Top face
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+
+		// Bottom face
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, 1.0,
+
+		// Right face
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+
+		// Left face
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
 	];
 
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-	const colors = [
-		1.0, 1.0, 1.0, 1.0,
-		1.0, 0.0, 0.0, 1.0,
-		0.0, 1.0, 0.0, 1.0,
-		0.0, 0.0, 1.0, 1.0
+	const faceColors = [
+		[1.0, 1.0, 1.0, 1.0],
+		[1.0, 0.0, 0.0, 1.0],
+		[0.0, 1.0, 0.0, 1.0],
+		[0.0, 0.0, 1.0, 1.0],
+		[1.0, 1.0, 0.0, 1.0],
+		[1.0, 0.0, 1.0, 1.0]
 	];
+
+	let colors: Array<number> = [];
+	for (let j = 0; j < faceColors.length; j++) {
+		const c = faceColors[j];
+		colors = colors.concat(c, c, c, c);
+	}
 
 	const colorBuffer = createBuffer(gl);
 	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
+	const indexBuffer = createBuffer(gl);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+	const indices = [
+		0, 1, 2, 0, 2, 3,
+		4, 5, 6, 4, 6, 7,
+		8, 9, 10, 8, 10, 11,
+		12, 13, 14, 12, 14, 15,
+		16, 17, 18, 16, 18, 19,
+		20, 21, 22, 20, 22, 23
+	];
+
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
 	return {
 		position: positionBuffer,
 		color: colorBuffer,
+		indices: indexBuffer,
 	}
 }
 
@@ -136,10 +190,11 @@ function drawScene(gl: WebGL2RenderingContext, programInfo: ProgramInfo, buffers
 
 	const modelViewMatrix = mat4.create();
 	mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
-	mat4.rotate(modelViewMatrix, modelViewMatrix, squareRotation, [0, 0, 1]);
+	mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation, [0, 0, 1]);
+	mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation * .7, [0, 1, 0]);
 
 	{
-		const numComponents = 2;
+		const numComponents = 3;
 		const type = gl.FLOAT;
 		const normalize = false;
 		const stride = 0;
@@ -166,13 +221,15 @@ function drawScene(gl: WebGL2RenderingContext, programInfo: ProgramInfo, buffers
 	gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
 	gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
 
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
 	{
+		const vertexCount = 36;
+		const type = gl.UNSIGNED_SHORT;
 		const offset = 0;
-		const vertexCount = 4;
-		gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+		gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
 	}
 
-	squareRotation += deltaTime;
+	cubeRotation += deltaTime;
 }
 
 
